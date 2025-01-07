420961895addd1154f825b7f69c505e287f8991e
SameerGangurde71 <sameergangurde71@gmail.com>
2025-01-07 10:55:05 +0530

    utils code discussion

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+
+export { asyncHandler }
+
+// const asyncHandler = () => {}
+// const asyncHandler = (funct) => () =>{}
+// const asyncHandler = (func) => async () => {}
+    //above 3 process is explanation of next step
+/*
+const asyncHandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    }
+    catch(error){
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+
+        })
+    }
+} */
